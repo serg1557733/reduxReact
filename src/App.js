1a962d69e55953bec51e0912ccf9ce1f4ee570d3
serg155alternate <serg1557733@ukr.net>
2022-01-09 17:45:06 +0200

    using redax with pure JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { createStore } from 'redux';
+
+
+const initialState = {value: 0}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'INC': 
+            return {...state,
+                    value: state.value + 1
+            };
+        case 'DEC': 
+        return {...state,
+            value: state.value - 1
+    };
+        case 'RND': 
+        return {...state,
+            value: state.value * action.payload
+    };
+        default: return state;
+    }
+    
+}
+
+const store = createStore(reducer);
+
+const update = () => {
+    document.getElementById('out').textContent = store.getState().value;
+}
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc());
+})
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec());
+})
+document.getElementById('rnd').addEventListener('click', () => {
+    let value = Math.floor(Math.random() * 10);
+    store.dispatch(rnd(value));
+})
+
+store.subscribe(update);
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'})
+const rnd = (value) => ({type: 'RND', payload: value})
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi world</h1>
     </div>
   );
 }
+
 
 export default App;
