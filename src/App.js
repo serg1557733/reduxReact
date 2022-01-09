d478402919af179d35da20464cd32a1cbce6afae
serg155alternate <serg1557733@ukr.net>
2022-01-09 18:13:07 +0200

    create reducer and action, create dispatches func, destruct store

--- src/App.js
+++ src/App.js
@@ -1,52 +1,37 @@
 
 import './App.css';
 import { createStore } from 'redux';
-
-
-const initialState = {value: 0}
-
-const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case 'INC': 
-            return {...state,
-                    value: state.value + 1
-            };
-        case 'DEC': 
-        return {...state,
-            value: state.value - 1
-    };
-        case 'RND': 
-        return {...state,
-            value: state.value * action.payload
-    };
-        default: return state;
-    }
-    
-}
+import { reducer } from './reducer';
+import { inc, dec, rnd } from './actions';
 
 const store = createStore(reducer);
 
+const {dispatch, subscribe, getState} = store;
+
+
 const update = () => {
-    document.getElementById('out').textContent = store.getState().value;
+    document.getElementById('out').textContent = getState().value;
 }
 
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc());
-})
 
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec());
-})
+
+const incDispatch = () => dispatch(inc());
+const rndDispatch = (value) => dispatch(rnd(value));
+const decDispatch = () => dispatch(dec());
+
+
+
+document.getElementById('inc').addEventListener('click', incDispatch);
+document.getElementById('dec').addEventListener('click', decDispatch);
 document.getElementById('rnd').addEventListener('click', () => {
     let value = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(value));
+    rndDispatch(value);
 })
 
-store.subscribe(update);
 
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'})
-const rnd = (value) => ({type: 'RND', payload: value})
+
+subscribe(update);
+
 
 
 
